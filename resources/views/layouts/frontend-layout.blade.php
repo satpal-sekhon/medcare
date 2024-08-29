cc2bad39e9a701a9a03455d5d75eee834b873bca
Satpal Singh Sekhon <satpalsekhon01@gmail.com>
2024-08-29 08:41:04 +0530

    Fixed header notifications issue

--- resources/views/layouts/frontend-layout.blade.php
+++ resources/views/layouts/frontend-layout.blade.php
@@ -54,28 +54,20 @@
         <div class="header-top">
             <div class="container-fluid-lg">
                 <div class="row">
-                    <div class="col-xxl-3 d-xxl-block d-none">
-                        {{-- <div class="top-left-header">
-                            <i class="iconly-Location icli text-white"></i>
-                            <span class="text-white">1418 Riverwood Drive, CA 96052, US</span>
-                        </div> --}}
-                    </div>
-
-                    <div class="col-xxl-6 col-lg-9 d-lg-block d-none">
+                    <div class="col-xxl-12 col-lg-12 d-lg-block d-none mx-auto">
                         <div class="header-offer">
                             <div class="notification-slider">
                                 <div>
-                                    <div class="timer-notification">
+                                    <div class="timer-notification text-center">
                                         <h6><strong class="me-1">Welcome to Medine Shop!</strong>Wrap new offers/gift
                                             every single day on Weekends.<strong class="ms-1">New Coupon Code: Med2589
                                             </strong>
-
                                         </h6>
                                     </div>
                                 </div>
 
                                 <div>
-                                    <div class="timer-notification">
+                                    <div class="timer-notification text-center">
                                         <h6>Extra 50% Off on All Generic Medicine
                                             <a href="javascript:void(0)" class="text-white">Buy Now
                                                 !</a>
