fee662d47ae6fccb81ff23a48355fcf4efc18165
Satpal Singh Sekhon <satpalsekhon01@gmail.com>
2024-09-16 09:01:54 +0530

    Fixed error

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -11,6 +11,6 @@ class OrderItem extends Model
     protected $guarded=[];
 
     public function product(){
-        return $this->belongsTos(Product::class);
+        return $this->belongsTo(Product::class);
     }
 }
