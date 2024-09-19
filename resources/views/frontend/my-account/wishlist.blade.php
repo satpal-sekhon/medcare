eb6a8f00f5a9b0355a0059ca6b2e0ca7e139b0b2
SatpalInfilogix <satpalinfilogix@gmail.com>
2024-09-19 18:05:04 +0530

    Added message for wishlist feature

--- resources/views/frontend/my-account/wishlist.blade.php
+++ resources/views/frontend/my-account/wishlist.blade.php
@@ -7,113 +7,11 @@
         <h2>My Wishlist History</h2>
         <span class="title-leaf title-leaf-gray">
             <svg class="icon-width bg-gray">
-                <use xlink:href="../assets/svg/leaf.svg#leaf"></use>
+                <use xlink:href="{{ asset('assets/svg/leaf.svg#leaf') }}"></use>
             </svg>
         </span>
     </div>
-    <div class="row g-sm-4 g-3">
-        <div class="col-xxl-3 col-lg-6 col-md-4 col-sm-6">
-            <div class="product-box-3 theme-bg-white h-100">
-                <div class="product-header">
-                    <div class="product-image">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/product/6.png') }}" class="img-fluid lazyload" alt="">
-                        </a>
 
-                        <div class="product-header-top">
-                            <button class="btn wishlist-button close_button">
-                                <i data-feather="x"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="product-footer">
-                    <div class="product-detail">
-                        <span class="span-name">Hygiene</span>
-                        <a href="#">
-                            <h5 class="name">Product 1 name</h5>
-                        </a>
-                        <h6 class="unit mt-1">550 G</h6>
-                        <h5 class="price">
-                            <span class="theme-color">₹14.25</span>
-                            <del>₹16.57</del>
-                        </h5>
-                        <div class="add-to-cart-box mt-2">
-                            <button class="btn btn-add-cart addcart-button" tabindex="0">Add
-                                <span class="add-icon">
-                                    <i class="fa-solid fa-plus"></i>
-                                </span>
-                            </button>
-                            <div class="cart_qty qty-box">
-                                <div class="input-group">
-                                    <button type="button" class="qty-left-minus" data-type="minus" data-field="">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                    <input class="form-control input-number qty-input" type="text" name="quantity"
-                                        value="0">
-                                    <button type="button" class="qty-right-plus" data-type="plus" data-field="">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-xxl-3 col-lg-6 col-md-4 col-sm-6">
-            <div class="product-box-3 theme-bg-white h-100">
-                <div class="product-header">
-                    <div class="product-image">
-                        <a href="#">
-                            <img src="{{ asset('assets/images/product/2.png') }}" class="img-fluid lazyload" alt="">
-                        </a>
-
-                        <div class="product-header-top">
-                            <button class="btn wishlist-button close_button">
-                                <i data-feather="x"></i>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="product-footer">
-                    <div class="product-detail">
-                        <span class="span-name">Hygiene</span>
-                        <a href="#">
-                            <h5 class="name">Prodct 2 name</h5>
-                        </a>
-                        <h6 class="unit mt-1">250 ml</h6>
-                        <h5 class="price">
-                            <span class="theme-color">₹08.02</span>
-                            <del>₹15.15</del>
-                        </h5>
-                        <div class="add-to-cart-box mt-2">
-                            <button class="btn btn-add-cart addcart-button" tabindex="0">Add
-                                <span class="add-icon">
-                                    <i class="fa-solid fa-plus"></i>
-                                </span>
-                            </button>
-                            <div class="cart_qty qty-box">
-                                <div class="input-group">
-                                    <button type="button" class="qty-left-minus" data-type="minus" data-field="">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                    <input class="form-control input-number qty-input" type="text" name="quantity"
-                                        value="0">
-                                    <button type="button" class="qty-right-plus" data-type="plus" data-field="">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <h4 class="text-center fw-bold text-success">Wishlist feature will be implemented soon</h4>
 </div>
 @endsection
